ef3f1186dcf78605a5713b9fe49673f05d3afada
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-06-15 15:18:28 -0700

    replaced L

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <h1>L</h1>
         </a>
       </div>
       <h1>Vite + React</h1>
